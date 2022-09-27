dec496b2d0a13d19a136d711b2a12b1bf9f9af11
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-09-27 19:46:53 +0200

    radv: implement a unified heap on APUs and enable for RDR2
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -358,6 +358,7 @@ struct radv_instance {
    bool disable_aniso_single_level;
    bool zero_vram;
    bool disable_sinking_load_input_fs;
+   bool enable_unified_heap_on_apu;
 };
 
 VkResult radv_init_wsi(struct radv_physical_device *physical_device);
