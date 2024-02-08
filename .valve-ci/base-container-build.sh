a05d3f005093d6b398c77125d651195c2a20e6e2
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2024-02-08 15:45:23 +0100

    ci: add support for building vkd3d-proton in the image
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- .valve-ci/base-container-build.sh
+++ .valve-ci/base-container-build.sh
@@ -2,12 +2,29 @@
 
 set -eux
 
+# Build vkd3d-proton
+echo "Building vkd3d-proton ('$VKD3D_PROTON_COMMIT')"
+
+# Install vkd3d-proton dependencies
+pacman --noconfirm -Suy meson wine-staging glslang
+
+git clone https://github.com/HansKristian-Work/vkd3d-proton.git --single-branch -b master --no-checkout /vkd3d-proton-src
+pushd /vkd3d-proton-src
+git checkout $VKD3D_PROTON_COMMIT
+git submodule update --init --recursive
+meson setup build -Denable_tests=true --buildtype release --prefix /vkd3d-proton-tests --strip
+ninja -C build install
+install -D -m755 -t /vkd3d-proton-tests/bin build/tests/d3d12
+popd
+
+rm -rf /vkd3d-proton-src
+
+# Build VKCTS
 echo "Building '$DEQP_BRANCH'"
 
-# VKCTS dependencies
+# Install VKCTS dependencies
 pacman --noconfirm -Suy git ninja cmake python3 libx11 libglvnd
 
-# Build vkcts
 git clone https://github.com/KhronosGroup/VK-GL-CTS.git -b $DEQP_BRANCH --depth 1 /VK-GL-CTS
 python3 /VK-GL-CTS/external/fetch_sources.py --insecure
 cmake -S /VK-GL-CTS -B /deqp -G Ninja \
