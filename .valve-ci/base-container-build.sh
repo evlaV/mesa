b774a7b402b7b9a2fc95b2e0fa7a0b410e23071d
Martin Roukala (né Peres) <martin.roukala@mupuf.org>
2024-10-28 10:16:56 +0200

    base-container: install pyYAML to build mesa

--- .valve-ci/base-container-build.sh
+++ .valve-ci/base-container-build.sh
@@ -67,7 +67,7 @@ cargo install --root /usr/local deqp-runner
 pacman --noconfirm -R rust
 
 # Mesa dependencies
-pacman --noconfirm -Suy git openssh python-mako libxml2 libx11 xorgproto libdrm libxshmfence wayland wayland-protocols zstd elfutils llvm libunwind libxrandr valgrind meson glslang
+pacman --noconfirm -Suy git openssh python-mako libxml2 libx11 xorgproto libdrm libxshmfence wayland wayland-protocols zstd elfutils llvm libunwind libxrandr valgrind meson glslang python-yaml
 
 # Clear the caches
 pacman --noconfirm -Scc
