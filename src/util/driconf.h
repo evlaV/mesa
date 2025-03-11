6059fbea9bb0c5d432ca02d4065cbab9803b5b74
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2025-03-11 17:38:12 +0100

    STEAMOS: radv: add radv_disable_ngg_culling and enable it for No Rest for the Wicked
    This fixes a performance regression since 8d97c3bd064
    ("radv: Increase maximum allowed PS params for enabling NGG culling.").
    
    This only changes the physical device cache key for this game, which
    implicitly disables shaders precompilation.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/util/driconf.h
+++ src/util/driconf.h
@@ -792,6 +792,9 @@
    DRI_CONF_OPT_B(radv_disable_hiz_his_gfx12, def, \
                   "Disable HiZ/HiS on GFX12 (RDNA4) to workaround a hw bug that causes random GPU hangs")
 
+#define DRI_CONF_RADV_DISABLE_NGG_CULLING(def) \
+   DRI_CONF_OPT_B(radv_disable_ngg_culling, def, "Disable NGG culling on GFX10.3.")
+
 /**
  * \brief ANV specific configuration options
  */
